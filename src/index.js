373985579f36278b1c530af6ed3206b9138922e4
nikpalyi <nikolett.palyi@wipro.com>
2019-08-01 16:10:39 +0100

    adding frist ACTION

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ import rootReducer from './reducers';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+import { addCharacterById } from './actions';
+
 const store = createStore(rootReducer);
 console.log('store.getState()', store.getState());
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(addCharacterById(2));
 
 ReactDOM.render(
   <Provider store={store}>
