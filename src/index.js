ab7ff83a4d2b82c8af13f92b05f91dbea1879514
nikpalyi <nikolett.palyi@wipro.com>
2019-08-01 15:23:59 +0100

    Store setup

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(rootReducer);
+console.log('store.getState()', store.getState());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
